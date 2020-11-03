dade9ebc216ff1903b3eb650730ea8b5325d3bac
Frank Bergmann <frank.bergmann@project-open.com>
2020-11-03 18:29:22 +0000

    - Updated to OpenACS 5.9.1

--- sql/oracle/content-folder.sql
+++ sql/oracle/content-folder.sql
@@ -284,7 +284,8 @@ procedure copy (
   target_folder_id	in cr_folders.folder_id%TYPE,
   creation_user		in acs_objects.creation_user%TYPE,
   creation_ip		in acs_objects.creation_ip%TYPE default null,
-  name                  in cr_items.name%TYPE default null
+  name                  in cr_items.name%TYPE default null,
+  label                 in cr_folders.label%TYPE default null  
 ) is
   v_valid_folders_p     integer := 0;
   v_current_folder_id   cr_folders.folder_id%TYPE;
@@ -363,7 +364,7 @@ begin
       v_new_folder_id := content_folder.new(
 	  parent_id     => copy.target_folder_id,
           name	        => nvl(copy.name,v_name),
-	  label	        => v_label,
+	  label	        => nvl(copy.label,v_label),
 	  description   => v_description,
 	  creation_user => copy.creation_user,
 	  creation_ip   => copy.creation_ip
--- sql/postgresql/upgrade/upgrade-5.9.0d1-5.9.0d2.sql
+++ sql/postgresql/upgrade/upgrade-5.9.0d1-5.9.0d2.sql
@@ -10,42 +10,42 @@
 -- functions although the create script do not define this triggers.
 -- It seems that an update script was missing.
 -- 
-DROP TRIGGER if exists cr_folder_del_ri_trg ON cr_items;
-DROP FUNCTION if exists cr_folder_del_ri_trg();
+DROP TRIGGER IF EXISTS cr_folder_del_ri_trg ON cr_items;
+DROP FUNCTION IF EXISTS cr_folder_del_ri_trg();
 
-DROP TRIGGER if exists cr_folder_ins_up_ri_trg ON cr_folders;
-DROP FUNCTION if exists cr_folder_ins_up_ri_trg();
+DROP TRIGGER IF EXISTS cr_folder_ins_up_ri_trg ON cr_folders;
+DROP FUNCTION IF EXISTS cr_folder_ins_up_ri_trg();
 
 -- 
 -- Handle latest_revision and live_revision via foreign keys
 --
 
--- fraber 161128: Delete inconsistent entries
-update cr_items set latest_revision = null 
+-- fraber 161128: Delete inconsistent entries                                                                                                       
+update cr_items set latest_revision = null
 where latest_revision in (select latest_revision from cr_items except select revision_id from cr_revisions);
 
-ALTER TABLE cr_items DROP CONSTRAINT if exists cr_items_latest_fk;
+
+ALTER TABLE cr_items DROP CONSTRAINT IF EXISTS cr_items_latest_fk;
 ALTER TABLE cr_items ADD CONSTRAINT cr_items_latest_fk
 FOREIGN KEY (latest_revision) REFERENCES cr_revisions(revision_id);
 
--- fraber 161128: Delete inconsistent entries
-update cr_items set live_revision = null 
+-- fraber 161128: Delete inconsistent entries                                                                                                       
+update cr_items set live_revision = null
 where live_revision in (select live_revision from cr_items except select revision_id from cr_revisions);
 
-ALTER TABLE cr_items DROP CONSTRAINT if exists cr_items_live_fk;
+ALTER TABLE cr_items DROP CONSTRAINT IF EXISTS cr_items_live_fk;
 ALTER TABLE cr_items ADD CONSTRAINT cr_items_live_fk
 FOREIGN KEY (live_revision) REFERENCES cr_revisions(revision_id);
 
 
-DROP TRIGGER if exists cr_revision_del_ri_tr on cr_items;
-DROP FUNCTION if exists cr_revision_del_ri_tr();
+DROP TRIGGER IF EXISTS cr_revision_del_ri_tr on cr_items;
+DROP FUNCTION IF EXISTS cr_revision_del_ri_tr();
 
-DROP TRIGGER if exists cr_revision_ins_ri_tr on cr_items;
-DROP FUNCTION if exists cr_revision_ins_ri_tr();
+DROP TRIGGER IF EXISTS cr_revision_ins_ri_tr on cr_items;
+DROP FUNCTION IF EXISTS cr_revision_ins_ri_tr();
 
-DROP TRIGGER if exists cr_revision_up_ri_tr on cr_items;
-DROP FUNCTION if exists cr_revision_up_ri_tr();
+DROP TRIGGER IF EXISTS cr_revision_up_ri_tr on cr_items;
+DROP FUNCTION IF EXISTS cr_revision_up_ri_tr();
 
-DROP TRIGGER if exists cr_revision_del_rev_ri_tr on cr_revisions;
-DROP FUNCTION if exists cr_revision_del_rev_ri_tr();
-
+DROP TRIGGER IF EXISTS cr_revision_del_rev_ri_tr on cr_revisions;
+DROP FUNCTION IF EXISTS cr_revision_del_rev_ri_tr();
